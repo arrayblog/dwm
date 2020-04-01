1bc93ce1c2207d17e2f6af53b8d0ae6ef2829d36
arrayblog <815520431@qq.com>
2020-04-01 18:31:47 +0800

    modify mapping key

--- config.h
+++ config.h
@@ -118,6 +118,9 @@ static Key keys[] = {
 	{ MODKEY,						XK_period, focusmon,	   {.i = +1 } },
 	{ MODKEY|ShiftMask,				XK_comma,  tagmon,		   {.i = -1 } },
 	{ MODKEY|ShiftMask,				XK_period, tagmon,		   {.i = +1 } },
+	{ MODKEY,                       XK_w,      hidewin,        {0} },
+	{ MODKEY|ShiftMask,             XK_w,      restorewin,     {0} },
+
 	TAGKEYS(						XK_1,					   0)
 	TAGKEYS(						XK_2,					   1)
 	TAGKEYS(						XK_3,					   2)
